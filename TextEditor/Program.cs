84a65fb28334275dd843fb4f1934251dcf582432
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-24 08:55:25 -0300

    Csharp

---
+++ TextEditor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer? ");
+            Console.WriteLine(" (1) -> Abrir arquivo \n (2) -> Criar arquivo  \n (0) -> Sair");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Open();
+                    break;
+                case 2:
+                    ToEdit();
+                    break;
+                case 0:
+                    System.Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Valor inválido.");
+                    break;
+            }
+        }
+
+        static void Open()
+        {
+
+        }
+
+        static void ToEdit()
+        {
+
+        }
+    }
+}
